53691ab6885afdea5c0ffa62097c0c0e960dc29c
SandraBender <sandraos15@ru.is>
2017-10-02 13:20:13 +0000

    classes

--- Project1.py
+++ Project1.py
@@ -34,11 +34,31 @@ array = movies.values
 X = array[:,0:8]
 Y = array[:,8]
 
+# Change the output to classes from 1-10 
+for y in range(0, len(Y)):
+	if 0 < Y[y] <= 1:
+		Y[y] = 1
+	if 1 < Y[y] <= 2:
+		Y[y] = 2
+	if 2 < Y[y] <= 3:
+		Y[y] = 3
+	if 3 < Y[y] <= 4:
+		Y[y] = 4
+	if 4 < Y[y] <= 5:
+		Y[y] = 5
+	if 5 < Y[y] <= 6:
+		Y[y] = 6
+	if 6 < Y[y] <= 7:
+		Y[y] = 7
+	if 7 < Y[y] <= 8:
+		Y[y] = 8
+	if 8 < Y[y] <= 9:
+		Y[y] = 9
+	if 9 < Y[y] <= 10:
+		Y[y] = 10
+
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.20, random_state=42)
 
-
-print(len(y_train))
-print(len(y_test))
 
 ############################# Other stuff #############################
