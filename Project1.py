4bebd7acf170b4c782bab54e494eeab2c994859a
juliaodds18 <juliaodds18@gmail.com>
2017-10-04 17:24:38 +0000

    Mushrooms added

--- Project1.py
+++ Project1.py
@@ -3,6 +3,9 @@ import scipy as sp
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from matplotlib.colors import ListedColormap
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import cross_val_score
 
 ############################# Preprocessing the dataset #############################
 
@@ -22,12 +25,16 @@ print(movies.describe())
 # Possible outcomes 
 print(len(movies.revenue.unique()))
 
+print(movies.spoken_languages.unique())
+
+
 # Normalize the data, delete useless columns 
 columnsToDrop = ['homepage', 'id', 'keywords','original_title', 'overview', 'popularity', 'release_date', 'status', 'tagline', 'title', 'vote_count']
 movies.drop(columnsToDrop, inplace=True, axis=1)
 
 # Print the columns after normalizing the data 
 print("attribute names: %s" %  movies.columns) 
+
 
 # Split the data into input and output 
 array = movies.values
@@ -62,3 +69,60 @@ X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.20, random_
 
 
 ############################# Other stuff #############################
+'''
+#K NEAREST NEIGHBOUR
+n_neighbors = 53
+
+h = .02  # step size in the mesh
+
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+listOfk = list(range(0,6000))
+neigh = list(filter(lambda x: x % 10 != 0, listOfk))
+
+# empty list that will hold cv scores
+cv_scores = []
+
+# Perform 10-fold cross validation
+# This is done to find the best value for k 
+for k in neigh:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
+    cv_scores.append(scores.mean())
+    print(scores.mean())
+    print(k)
+
+# This is the best value for k, at least on the range that we tested
+print(max(cv_scores))
+
+# Create the graph to display the classification
+for weights in ['uniform', 'distance']:
+    # Create an instance of the Neighbours Classifier and fit the data
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X_train, y_train)
+
+    # Plot the decision boundary. Assign a colour to each point in the mesh
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification (k = %i, weights = '%s')"
+              % (n_neighbors, weights))
+
+# Print the plot
+plt.show()
+'''
